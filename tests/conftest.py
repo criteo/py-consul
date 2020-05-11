ddc2545c632e3d9df868714af4c3d30df1739c45
Bastien Vallet <b.vallet@criteo.com>
2020-05-11 14:24:01 +0200

    [compat] Drop support of deprecated python2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import collections
 import subprocess
 import platform
-import sys
 import tempfile
 import socket
 import shlex
@@ -14,17 +13,6 @@ import pytest
 import py
 
 collect_ignore = []
-
-if sys.version_info[0] < 3:
-    p = os.path.join(os.path.dirname(__file__), 'test_aio.py')
-    collect_ignore.append(p)
-
-
-if sys.version_info[0] == 2 and sys.version_info[1] < 7:
-    p = os.path.join(os.path.dirname(__file__), 'test_twisted.py')
-    collect_ignore.append(p)
-    p = os.path.join(os.path.dirname(__file__), 'test_tornado.py')
-    collect_ignore.append(p)
 
 
 def get_free_ports(num, host=None):
