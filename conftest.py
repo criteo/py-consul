7167591f307aca460fc902e1fdcdff7b80760ce7
Mathias Brulatout <m.brulatout@criteo.com>
2024-08-26 19:03:45 +0200

    fix(avoid) some more flakyness

--- conftest.py
+++ conftest.py
@@ -166,11 +166,11 @@ def consul_port(consul_instance):
 @pytest.fixture
 def acl_consul(acl_consul_instance):
     instance, token = acl_consul_instance
-    return ACLConsul(Consul(port=instance.port), token, instance.version)
+    return ACLConsul(Consul(consistency="consistent", port=instance.port), token, instance.version)
 
 
 @pytest.fixture
 def consul_obj(consul_port):
     consul_port, consul_version = consul_port
-    c = Consul(port=consul_port)
+    c = Consul(consistency="consistent", port=consul_port)
     return c, consul_version
