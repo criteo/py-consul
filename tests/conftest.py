76b94b2c08fb441a8109f19858eec7a5573cd996
Mathias Brulatout <m.brulatout@criteo.com>
2024-08-27 09:32:44 +0200

    remove consistent hack

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,11 +10,11 @@ ACLConsul = collections.namedtuple("ACLConsul", ["instance", "token", "version"]
 @pytest.fixture
 def acl_consul(acl_consul_instance):
     instance, token = acl_consul_instance
-    return ACLConsul(Consul(consistency="consistent", port=instance.port), token, instance.version)
+    return ACLConsul(Consul(port=instance.port), token, instance.version)
 
 
 @pytest.fixture
 def consul_obj(consul_port):
     consul_port, consul_version = consul_port
-    c = Consul(consistency="consistent", port=consul_port)
+    c = Consul(port=consul_port)
     return c, consul_version
